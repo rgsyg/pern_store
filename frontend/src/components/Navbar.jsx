f368b4591b285c9e48f6032632b4079618ad3107
rigs <rigorsyguatbsn@gmail.com>
2025-06-08 17:51:18 +0800

    initial commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Link, useResolvedPath } from "react-router-dom";
+import { ShoppingBag, ShoppingCartIcon } from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
+import { useProductStore } from "../store/useProductStore";
+
+export default function Navbar() {
+  const { pathname } = useResolvedPath();
+  const isHomePage = pathname === "/";
+  const { products } = useProductStore();
+
+  return (
+    <div className="bg-base-300 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="navbar px-4 min-h-[4rem] justify-between">
+          <div className="flex-1 lg:flex-nones">
+            <Link to="/" className="hover:opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <ShoppingCartIcon className="size-9 text-primary" />
+                <span className="font-semibold font-mono tracking-widest text-2xl bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
+                  PERNSTORE
+                </span>
+              </div>
+            </Link>
+          </div>
+
+          {/* RIGHT SECTION */}
+          <div className="flex items-center gap-4">
+            <ThemeSelector />
+            {isHomePage && (
+              <div className="indicator">
+                <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
+                  <ShoppingBag className="size-5" />
+                  <span className="badge badge-sm badge-primary indicator-item">
+                    {products.length}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
